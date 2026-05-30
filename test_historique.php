7217395c5d1abe15be242db6e4987298ffa3bb9a
Timothé Chamebraud <timothe.chamberaud@gmail.com>
2026-05-30 11:51:31 +0200

    chore: supprimer les scripts de test temporaires a la racine

--- test_historique.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/backend/config/Database.php';
-require_once __DIR__ . '/backend/controllers/NegociationController.php';
-
-$db = new Database();
-$conn = $db->getConnection();
-$controller = new NegociationController($conn);
-
-$controller->getHistorique(10);
